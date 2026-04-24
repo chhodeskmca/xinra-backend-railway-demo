29cc4cf563a22fd77e704cefce8a0fcc6b815330
chhodeskmca <75718861+chhodeskmca@users.noreply.github.com>
2026-04-24 17:41:13 +0530

    Refactor QR scan URL generation
    Updated QR scan URL to remove venue path prefix.

--- src/shared/utils/venueQr.js
+++ src/shared/utils/venueQr.js
@@ -1,14 +1,12 @@
 const { randomBytes } = require('node:crypto');
 const { env } = require('../../config/env');
 
-const VENUE_SCAN_PATH = '/venue/scan';
-
 function generateQrToken() {
   return randomBytes(32).toString('hex');
 }
 
 function buildQrScanUrl(qrToken) {
-  const scanPath = `${VENUE_SCAN_PATH}/${qrToken}`;
+  const scanPath = `/${qrToken}`;
 
   if (!env.frontendBaseUrl) {
     return scanPath;
